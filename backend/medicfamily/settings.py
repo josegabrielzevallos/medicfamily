b511f930aed6662cf5d97f9ed41b4e4162d30579
Jose Gabriel Zevallos Delgado de la Flor <jose.zevallos.delgadolaflor@gmail.com>
2026-03-18 17:03:10 -0700

    configuration od back end in cloud

--- backend/medicfamily/settings.py
+++ backend/medicfamily/settings.py
@@ -33,9 +33,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',  # Sirve archivos estáticos en producción
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
